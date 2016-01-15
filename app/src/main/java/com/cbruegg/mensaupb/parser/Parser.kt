92d101c396cdb9cee2677e656c2503002bf5f812
Christian <c.brueggemann94@gmail.com>
2016-01-15 10:19:16 +0100

    Fix restaurant parser

--- app/src/main/java/com/cbruegg/mensaupb/parser/Parser.kt
+++ app/src/main/java/com/cbruegg/mensaupb/parser/Parser.kt
@@ -2,22 +2,19 @@ package com.cbruegg.mensaupb.parser
 
 import com.cbruegg.mensaupb.model.Dish
 import com.cbruegg.mensaupb.model.Restaurant
-import com.squareup.moshi.Json
 import okio.BufferedSource
 
 /**
  * Parse restaurants from the API response.
  */
 public fun parseRestaurantsFromApi(restaurantListSource: BufferedSource): List<Restaurant> =
-        MoshiProvider.provideJsonAdapter<Map<String, RestaurantInternal>>()
+        MoshiProvider.provideJsonAdapter<Map<String, Map<String, *>>>()
                 .fromJson(restaurantListSource)
-                .map { Restaurant(it.key, it.value.name, it.value.location, it.value.isActive) }
+                .map {
+                    Restaurant(it.key, it.value["name"] as String, it.value["location"] as String, it.value["active"] as Boolean)
+                }
 
 /**
  * Parse dishes from the API response.
  */
 public fun parseDishes(dishSource: BufferedSource): List<Dish> = MoshiProvider.provideListJsonAdapter<Dish>().fromJson(dishSource)
-
-private class RestaurantInternal(@Json(name = "name") val name: String,
-                                 @Json(name = "location") val location: String,
-                                 @Json(name = "active") val isActive: Boolean)
