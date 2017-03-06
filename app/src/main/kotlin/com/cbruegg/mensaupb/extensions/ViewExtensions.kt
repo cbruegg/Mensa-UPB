17f3d5a1949bb2271a5f512bffdaf53eee037c33
Christian <c.brueggemann94@gmail.com>
2017-03-06 16:17:53 +0100

    Remove unused code

--- app/src/main/kotlin/com/cbruegg/mensaupb/extensions/ViewExtensions.kt
+++ app/src/main/kotlin/com/cbruegg/mensaupb/extensions/ViewExtensions.kt
@@ -1,24 +1,6 @@
 package com.cbruegg.mensaupb.extensions
 
 import android.support.v4.widget.DrawerLayout
-import android.view.View
-import android.view.ViewGroup
-
-/**
- * Sequence of all direct children of the ViewGroup.
- * This is a non-recursive operation.
- */
-val ViewGroup.children: Sequence<View>
-    get() {
-        var i = 0
-        return generateSequence {
-            if (i < childCount) {
-                getChildAt(i++)
-            } else {
-                null
-            }
-        }
-    }
 
 /**
  * Switch the drawer state (closed/open).
