11fc8db998b672443bce898075cd2b2f43c23a38
cbruegg <mail@cbruegg.com>
2017-03-12 01:08:10 +0100

    Prefer top level functions

--- app/src/main/kotlin/com/cbruegg/mensaupb/util/ViewUtils.kt
+++ app/src/main/kotlin/com/cbruegg/mensaupb/util/ViewUtils.kt
@@ -2,14 +2,12 @@ package com.cbruegg.mensaupb.util
 
 import android.content.res.Resources
 
-object ViewUtils {
-    /**
-     * Convert dp to pixels.
-     */
-    fun dpToPx(dp: Int): Int = dp * Resources.getSystem().displayMetrics.density.toInt()
+/**
+ * Convert dp to pixels.
+ */
+fun dpToPx(dp: Int): Int = dp * Resources.getSystem().displayMetrics.density.toInt()
 
-    /**
-     * Convert pixels to dp.
-     */
-    fun pxToDp(px: Int) = px / Resources.getSystem().displayMetrics.density.toInt()
-}
+/**
+ * Convert pixels to dp.
+ */
+fun pxToDp(px: Int) = px / Resources.getSystem().displayMetrics.density.toInt()
