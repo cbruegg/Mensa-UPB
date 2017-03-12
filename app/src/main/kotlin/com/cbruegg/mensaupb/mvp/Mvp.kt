64267fcb981b60adb5c1cd7e08a3632943d73df8
cbruegg <mail@cbruegg.com>
2017-03-12 14:17:46 +0100

    Add MVP documentation

--- app/src/main/kotlin/com/cbruegg/mensaupb/mvp/Mvp.kt
+++ app/src/main/kotlin/com/cbruegg/mensaupb/mvp/Mvp.kt
@@ -5,8 +5,25 @@ import com.cbruegg.mensaupb.util.weakReference
 
 interface MvpView
 interface MvpModel
+
+/**
+ * The presenter will handle state management for you,
+ * but you need to implement this interface for your model
+ * to save and restore state to/from a [Bundle]. Android may discard
+ * the saved state after process death, so critical state that
+ * should survive app relaunches should be saved in other ways,
+ * for example by using a [com.cbruegg.mensaupb.util.delegates.PersistentPropertyDelegate]
+ * in your [MvpModel].
+ */
 interface MvpModelSaver<in M : MvpModel> {
+    /**
+     * @see [MvpModelSaver]
+     */
     fun save(model: M, savedInstanceState: Bundle)
+
+    /**
+     * @see [MvpModelSaver]
+     */
     fun restore(savedInstanceState: Bundle, intoModel: M)
 }
 
@@ -58,8 +75,29 @@ abstract class ModelMvpPresenter<V : MvpView, out M : MvpModel>(
         )
     }
 
+    /**
+     * Called when the view is attached to the presenter and the
+     * model state has been restored.
+     * You should not initialize any content of the view here.
+     *
+     * @see [initView]
+     */
     protected open fun onViewAttached() {}
+
+    /**
+     * Called after the view has been attached and the model state has
+     * been restored.
+     * You should load content into the View here. This method is usually
+     * called after methods like [android.app.Activity.onCreate] and
+     * [android.support.v4.app.Fragment.onCreateView], but not after methods
+     * like [android.app.Activity.onResume].
+     */
     protected open fun initView() {}
+
+    /**
+     * Called when the view is detached. This can mean
+     * that the UI is paused, but it may as well be destroyed.
+     */
     protected open fun onViewDetached() {}
 }
 
