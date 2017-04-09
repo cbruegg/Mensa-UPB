1b5a1bc74f4249e1ac7a362edcb934b762cb8e83
cbruegg <mail@cbruegg.com>
2017-04-09 18:51:34 +0200

    Performance improvements

--- app/src/main/kotlin/com/cbruegg/mensaupb/viewmodel/DataBindingProperty.kt
+++
@@ -1,8 +0,0 @@
-package com.cbruegg.mensaupb.viewmodel
-
-/**
- * Annotation used to mark properties as being used
- * in an XML layout for data binding. Used to
- * suppress warnings about unused properties.
- */
-@Retention(AnnotationRetention.SOURCE) annotation class DataBindingProperty
--- app/src/main/kotlin/com/cbruegg/mensaupb/viewmodel/DishViewModel.kt
+++ app/src/main/kotlin/com/cbruegg/mensaupb/viewmodel/DishViewModel.kt
@@ -3,34 +3,34 @@ package com.cbruegg.mensaupb.viewmodel
 import android.content.Context
 import com.cbruegg.mensaupb.R
 import com.cbruegg.mensaupb.cache.DbDish
+import com.cbruegg.mensaupb.compat.Html
 import com.cbruegg.mensaupb.extensions.capitalizeFirstChar
 import com.cbruegg.mensaupb.extensions.replace
 import com.cbruegg.mensaupb.model.PriceType
 import com.cbruegg.mensaupb.model.UserType
 import java.text.DecimalFormat
 import java.util.*
-import kotlin.comparisons.compareByDescending
-import kotlin.comparisons.thenBy
-import kotlin.comparisons.thenComparator
 
 /**
  * Wrapper for [DbDish] objects providing easy access to various attributes for data binding.
  */
-data class DishViewModel(@DataBindingProperty val dish: DbDish,
-                         @DataBindingProperty val headerText: String?,
-                         @DataBindingProperty val userPrice: Double,
-                         @DataBindingProperty val priceText: String,
-                         @DataBindingProperty val allergensText: String,
-                         @DataBindingProperty val badgesText: String?,
-                         @DataBindingProperty val position: Int,
-                         @DataBindingProperty val name: String) {
-    @DataBindingProperty val hasBadges = dish.badges.filterNotNull().isNotEmpty()
-    @DataBindingProperty val localizedCategory = dish.displayCategory()
-    @DataBindingProperty val containsAllergens = dish.allergens.isNotEmpty()
-    @DataBindingProperty val hasThumbnail = !dish.thumbnailImageUrl.isNullOrEmpty()
-    @DataBindingProperty val hasBigImage = !dish.imageUrl.isNullOrEmpty()
-    @DataBindingProperty val hasHeader = headerText != null
-    @DataBindingProperty val showDivider = hasHeader && position > 0
+data class DishViewModel(
+        val dish: DbDish,
+        val headerText: String?,
+        val userPrice: Double,
+        val priceText: String,
+        val allergensText: String,
+        val badgesText: String?,
+        val position: Int,
+        val name: String,
+        val description: CharSequence
+) {
+    val hasBadges = dish.badges.filterNotNull().isNotEmpty()
+    val containsAllergens = dish.allergens.isNotEmpty()
+    val hasThumbnail = !dish.thumbnailImageUrl.isNullOrEmpty()
+    val hasBigImage = !dish.imageUrl.isNullOrEmpty()
+    val hasHeader = headerText != null
+    val showDivider = hasHeader && position > 0
 }
 
 private val NUMBER_FORMAT = DecimalFormat("0.00")
@@ -48,7 +48,8 @@ private fun DbDish.toDishViewModel(headerText: String?, userType: UserType, cont
             .joinTo(buffer = StringBuilder(), transform = { context.getString(it.descriptionId) })
             .toString()
             .capitalizeFirstChar()
-    return DishViewModel(this, headerText, userPrice, priceText, allergensText, badgesText, position, this.displayName())
+    val description = Html.fromHtml(context.getString(R.string.row_dish_description, name, priceText, badgesText)).trim()
+    return DishViewModel(this, headerText, userPrice, priceText, allergensText, badgesText, position, this.displayName(), description)
 }
 
 
