ddcf9b5ae5732ca72bd63ddae9488d0553cde2e5
Christian Brüggemann <mail@cbruegg.com>
2017-09-17 18:08:03 +0200

    Add debug info to Parser

--- app/src/main/kotlin/com/cbruegg/mensaupb/parser/Parser.kt
+++ app/src/main/kotlin/com/cbruegg/mensaupb/parser/Parser.kt
@@ -11,22 +11,28 @@ import okio.BufferedSource
 /**
  * Parse restaurants from the API response.
  */
-fun parseRestaurantsFromApi(restaurantListSource: BufferedSource): List<Restaurant> =
-        MoshiProvider.provideJsonAdapter<Map<String, Map<String, *>>>()
-                .fromJson(restaurantListSource)!!
-                .map {
-                    Restaurant(it.key, it.value["name"] as String, it.value["location"] as String, it.value["active"] as Boolean)
-                }
+fun parseRestaurantsFromApi(restaurantListSource: BufferedSource): List<Restaurant> {
+    val moshi = MoshiProvider.provideJsonAdapter<Map<String, Map<String, *>>>()
+    val deserialized =
+            if (BuildConfig.DEBUG) {
+                val restaurantListStr = restaurantListSource.readString(Charsets.UTF_8)
+                Log.d("parseRestaurantsFromApi", restaurantListStr)
+                moshi.fromJson(restaurantListStr)
+            } else moshi.fromJson(restaurantListSource)
+    return deserialized!!.map {
+        Restaurant(it.key, it.value["name"] as String, it.value["location"] as String, it.value["active"] as Boolean)
+    }
+}
 
 /**
  * Parse dishes from the API response.
  */
 fun parseDishes(dishSource: BufferedSource): List<Dish> {
-    if (BuildConfig.DEBUG) {
-        return dishSource.use {
+    return if (BuildConfig.DEBUG) {
+        dishSource.use {
             val dishesStr = dishSource.readUtf8()
             Log.d("parseDishes", dishesStr)
             MoshiProvider.provideListJsonAdapter<Dish>().fromJson(dishesStr)!!
         }
-    } else return MoshiProvider.provideListJsonAdapter<Dish>().fromJson(dishSource)!!
+    } else MoshiProvider.provideListJsonAdapter<Dish>().fromJson(dishSource)!!
 }
