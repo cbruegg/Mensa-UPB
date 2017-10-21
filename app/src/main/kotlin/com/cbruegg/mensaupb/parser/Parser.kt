7d3bfa6725dee18c6b4e2dbaeba48994b1b69fdf
Christian Brüggemann <mail@cbruegg.com>
2017-10-21 15:21:16 +0200

    Use mensa common lib directly for serialization

--- app/src/main/kotlin/com/cbruegg/mensaupb/parser/Parser.kt
+++ app/src/main/kotlin/com/cbruegg/mensaupb/parser/Parser.kt
@@ -4,19 +4,18 @@ import com.cbruegg.mensaupbservice.api.Dish
 import com.cbruegg.mensaupbservice.api.DishesServiceResult
 import com.cbruegg.mensaupbservice.api.Restaurant
 import com.cbruegg.mensaupbservice.api.RestaurantsServiceResult
-import kotlinx.serialization.protobuf.ProtoBuf
 import okio.BufferedSource
 
 /**
  * Parse restaurants from the API response.
  */
 fun parseRestaurantsFromApi(restaurantListSource: BufferedSource): List<Restaurant> {
-    return ProtoBuf.loads<RestaurantsServiceResult>(restaurantListSource.readUtf8()).restaurants
+    return RestaurantsServiceResult.deserialize(restaurantListSource.readUtf8()).restaurants
 }
 
 /**
  * Parse dishes from the API response.
  */
 fun parseDishes(dishSource: BufferedSource): List<Dish> {
-    return ProtoBuf.loads<DishesServiceResult>(dishSource.readUtf8()).dishes
+    return DishesServiceResult.deserialize(dishSource.readUtf8()).dishes
 }
