01e14e4156b594935c6180bc9da5f4334006be21
Christian <mail@cbruegg.com>
2020-11-15 14:50:51 +0100

    Update some libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,35 +63,35 @@ android {
 tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
     kotlinOptions {
         freeCompilerArgs += "-Xuse-experimental=kotlin.Experimental"
-        allWarningsAsErrors = true
+        // allWarningsAsErrors = true
         jvmTarget ="1.8"
     }
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.recyclerview:recyclerview:1.0.0")
-    implementation("com.google.android.material:material:1.1.0-alpha10")
-    implementation("androidx.preference:preference:1.1.0")
-    implementation("androidx.browser:browser:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("com.google.android.material:material:1.2.1")
+    implementation("androidx.preference:preference:1.1.1")
+    implementation("androidx.browser:browser:1.2.0")
     implementation("androidx.annotation:annotation:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta2")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
-    implementation("com.squareup.okhttp3:okhttp:4.2.0")
+    implementation("com.squareup.okhttp3:okhttp:4.5.0")
     implementation("com.squareup.retrofit2:retrofit:2.6.1")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.4.0")
-    implementation("androidx.core:core-ktx:1.1.0")
-    implementation("androidx.fragment:fragment-ktx:1.1.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.1.0")
-    implementation("androidx.viewpager2:viewpager2:1.0.0-beta04")
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.fragment:fragment-ktx:1.2.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0-alpha01")
 
     implementation("com.cbruegg.mensaupbservice-common:mensaupbservice-common:1.1.1")
 
     val arrowVersion = "0.8.1"
     implementation("io.arrow-kt:arrow-core:$arrowVersion")
 
-    val daggerVersion = "2.24"
+    val daggerVersion = "2.29.1"
     kapt("com.google.dagger:dagger-compiler:$daggerVersion")
     implementation("com.google.dagger:dagger:$daggerVersion")
     compileOnly("javax.annotation:jsr250-api:1.0")
@@ -108,7 +108,7 @@ dependencies {
     implementation("io.requery:requery-kotlin:$requeryVersion")
     kapt("io.requery:requery-processor:$requeryVersion")
 
-    val glideVersion = "4.10.0"
+    val glideVersion = "4.11.0"
     implementation("com.github.bumptech.glide:glide:$glideVersion")
     implementation("com.github.bumptech.glide:okhttp3-integration:$glideVersion")
     implementation("com.github.bumptech.glide:recyclerview-integration:$glideVersion") {
@@ -124,18 +124,18 @@ dependencies {
     debugImplementation("com.facebook.stetho:stetho:$stethoVersion")
     debugImplementation("com.facebook.stetho:stetho-okhttp3:$stethoVersion")
 
-    val aarchVersion = "2.1.0"
+    val aarchVersion = "2.2.0"
     implementation("androidx.lifecycle:lifecycle-runtime:$aarchVersion")
     implementation("androidx.lifecycle:lifecycle-extensions:$aarchVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$aarchVersion")
     kapt("androidx.lifecycle:lifecycle-compiler:$aarchVersion")
 
-    implementation("androidx.work:work-runtime-ktx:2.2.0")
+    implementation("androidx.work:work-runtime-ktx:2.4.0")
 
     androidTestImplementation("org.jetbrains.kotlin:kotlin-test:${KotlinCompilerVersion.VERSION}")
     androidTestImplementation("junit:junit:4.12")
-    androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test:rules:1.2.0")
+    androidTestImplementation("androidx.test:runner:1.3.0")
+    androidTestImplementation("androidx.test:rules:1.3.0")
     androidTestImplementation("androidx.annotation:annotation:1.1.0")
     androidTestImplementation("org.mockito:mockito-android:2.28.2")
 }
