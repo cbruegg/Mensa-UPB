c7aefdb9b88d072de5ead3033f5140f02be538ef
cbruegg <mail@cbruegg.com>
2026-01-17 16:13:34 +0100

    Fix edge-to-edge support in PreferenceActivity

--- app/src/main/kotlin/com/cbruegg/mensaupb/activity/PreferenceActivity.kt
+++ app/src/main/kotlin/com/cbruegg/mensaupb/activity/PreferenceActivity.kt
@@ -2,11 +2,18 @@ package com.cbruegg.mensaupb.activity
 
 import android.content.Context
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.updatePadding
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.cbruegg.mensaupb.R
 import com.cbruegg.mensaupb.model.UserType
+import com.google.android.material.appbar.MaterialToolbar
 
 /**
  * Activity managing the user preferences.
@@ -19,10 +26,47 @@ class PreferenceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.activity_preference)
 
-        supportFragmentManager.beginTransaction()
-            .replace(android.R.id.content, PreferenceFragment())
-            .commit()
+        val toolbar = findViewById<MaterialToolbar>(R.id.preference_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        val root = findViewById<View>(R.id.preference_root)
+        val container = findViewById<View>(R.id.preference_container)
+        ViewCompat.setOnApplyWindowInsetsListener(root) { _, insets ->
+            val systemBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+            )
+            toolbar.updatePadding(
+                left = systemBars.left,
+                top = systemBars.top,
+                right = systemBars.right
+            )
+            container.updatePadding(
+                left = systemBars.left,
+                right = systemBars.right,
+                bottom = systemBars.bottom
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+        ViewCompat.requestApplyInsets(root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.preference_container, PreferenceFragment())
+                .commit()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        android.R.id.home -> {
+            finish()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     class PreferenceFragment : PreferenceFragmentCompat() {
