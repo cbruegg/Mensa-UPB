ddd127715c7dfbc93008aa85a4e16769632a49e5
cbruegg <mail@cbruegg.com>
2026-01-17 17:38:38 +0100

    AGP update

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.11.0")
+        classpath("com.android.tools.build:gradle:8.12.3")
         classpath(kotlin("gradle-plugin", "2.2.0"))
         classpath("com.google.gms:google-services:4.4.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.4")
